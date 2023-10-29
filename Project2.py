5704d1597dcd8ac6c9a73d0d0e6607b402056a85
Abel Atuan <abeleatuan@gmail.com>
2023-10-29 13:39:59 -0600

    Abel's Part

---
+++ Project2.py
@@ -0,0 +1,25 @@
+import math
+# Project 2 - Calc 1
+
+# 3
+x = 1
+
+output3 = 6*x-6
+
+print("The slope of the equation at the point x = " + str(x) + " is " + str(output3))
+
+# 4
+
+A = float(3)
+B = float(-4)
+C = float(1)
+
+def QuadraticFormula():
+    numerator1 = float(-B + math.sqrt(B**2 -4*A*C))
+    numerator2 = float(-B - math.sqrt(B**2 -4*A*C))
+
+    Result1 = numerator1/(2*A)
+    Result2 = numerator2/(2*A)
+
+    print("The first root is: " + str(Result1))
+    print("The second root is: " + str(Result2))
